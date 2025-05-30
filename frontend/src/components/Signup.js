ee55ee3cdd8ad5b940ef67486dad3009520e711a
Sandunikaamarasekara2001 <160486496+Sandunikaamarasekara2001@users.noreply.github.com>
2025-05-30 13:32:19 +0530

    Update backend API URLs to use EC2 public IP again

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -22,7 +22,7 @@ function Signup() {
     e.preventDefault();
 
     try {
-      const res = await axios.post('http://13.127.78.245:3007/api/auth/login', formData);
+      const res = await axios.post('http://3.200.243.107:3007/api/auth/login', formData);
       setMsg(res.data.message);
       alert("Login successful!");
     } catch (err) {
