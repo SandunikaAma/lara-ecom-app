3f0484f864b33bcf8e3ff2d698e796d6560dac1f
Sandunikaamarasekara2001 <160486496+Sandunikaamarasekara2001@users.noreply.github.com>
2025-05-30 21:15:17 +0530

    commit wait for it changes

---
+++ server/wait-for-it.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# wait-for-it.sh
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until nc -z "$host" 3306; do
+  echo "Waiting for MySQL at $host:3306..."
+  sleep 2
+done
+
+exec $cmd
